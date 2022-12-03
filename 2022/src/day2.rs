d003e63484303dc0e73ccb079faa84729d51cd44
Azkellas <e.desbois@hotmail.fr>
2022-12-03 10:12:26 +0100

    day3 2022

--- 2022/src/day2.rs
+++ 2022/src/day2.rs
@@ -1,31 +1,38 @@
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<(i32, i32)> {
-    input.lines().into_iter().map(|line| {
-        let line = line.as_bytes();
-        let opp_play = (line[0] - b'A') as i32;
-        let unknown = (line[2] - b'X') as i32;
-        (opp_play, unknown)
-    })
-    .collect()
+    input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line = line.as_bytes();
+            let opp_play = (line[0] - b'A') as i32;
+            let unknown = (line[2] - b'X') as i32;
+            (opp_play, unknown)
+        })
+        .collect()
 }
 
 #[aoc(day2, part1)]
 pub fn part1(input: &Vec<(i32, i32)>) -> i32 {
-    input.into_iter().map(|(opp_play, my_play)| {
-        // hand score + result score
-        (my_play + 1) + (3 * ((my_play - opp_play + 4) % 3))
-    })
-    .sum()
+    input
+        .into_iter()
+        .map(|(opp_play, my_play)| {
+            // hand score + result score
+            (my_play + 1) + (3 * ((my_play - opp_play + 4) % 3))
+        })
+        .sum()
 }
 
 #[aoc(day2, part2)]
 pub fn part2(input: &Vec<(i32, i32)>) -> i32 {
-    input.into_iter().map(|(opp_play, result)| {
-        let my_play = (opp_play + result + 2) % 3;
-        // hand score + result score
-        (my_play + 1) + (3 * ((my_play - opp_play + 4) % 3))
-    })
-    .sum()
+    input
+        .into_iter()
+        .map(|(opp_play, result)| {
+            let my_play = (opp_play + result + 2) % 3;
+            // hand score + result score
+            (my_play + 1) + (3 * ((my_play - opp_play + 4) % 3))
+        })
+        .sum()
 }
 
 #[cfg(test)]
---
+++ 2022/src/day3.rs
@@ -0,0 +1,65 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
+#[aoc(day3, part1)]
+pub fn part1(input: &str) -> i32 {
+    input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let (left, right) = line.trim().split_at(line.len() / 2);
+            let left: HashSet<char> = HashSet::from_iter(left.chars().into_iter());
+            let right: HashSet<char> = HashSet::from_iter(right.chars().into_iter());
+            let c = HashSet::intersection(&left, &right).next().unwrap();
+            match c.is_ascii_lowercase() {
+                true => (*c as u8 - b'a' + 1) as i32,
+                false => (*c as u8 - b'A' + 27) as i32,
+            }
+        })
+        .sum()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .into_iter()
+        .map(|l| HashSet::from_iter(l.chars().into_iter()))
+        .tuples()
+        .map(
+            |(l1, l2, l3): (HashSet<char>, HashSet<char>, HashSet<char>)| {
+                let c = HashSet::intersection(&l1, &l2)
+                    .filter(|c| l3.contains(c))
+                    .next()
+                    .unwrap();
+                match c.is_ascii_lowercase() {
+                    true => (*c as u8 - b'a' + 1) as i32,
+                    false => (*c as u8 - b'A' + 27) as i32,
+                }
+            },
+        )
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE: &str = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(EXAMPLE), 157);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(EXAMPLE), 70);
+    }
+}
