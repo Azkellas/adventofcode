05532c983e2a6c6c4a2f196f8a3b38c8188032ad
Azkellas <e.desbois@hotmail.fr>
2022-12-04 15:52:36 +0100

    Replace regex by scan_fmt to clean day4

--- 2022/src/day4.rs
+++ 2022/src/day4.rs
@@ -1,28 +1,22 @@
-use itertools::Itertools;
-use regex::Regex;
+use scan_fmt::*;
 
-#[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Vec<(i32, i32, i32, i32)> {
-    let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
-    re.captures_iter(input)
-        .flat_map(|c| c.iter().skip(1).collect::<Vec<_>>())
-        .map(|opt| opt.unwrap().to_owned().as_str().parse::<i32>().unwrap())
-        .tuples()
-        .collect()
+pub fn input_generator<'a>(input: &'a str) -> impl Iterator<Item = (i32, i32, i32, i32)> + 'a {
+    input
+        .lines()
+        .into_iter()
+        .map(|line| scan_fmt!(line, "{d}-{d},{d}-{d}", i32, i32, i32, i32).unwrap())
 }
 
 #[aoc(day4, part1)]
-pub fn part1(input: &Vec<(i32, i32, i32, i32)>) -> usize {
-    input
-        .into_iter()
+pub fn part1<'a>(input: &str) -> usize {
+    input_generator(input)
         .filter(|(a1, a2, b1, b2)| a1 <= b1 && a2 >= b2 || b1 <= a1 && b2 >= a2)
         .count()
 }
 
 #[aoc(day4, part2)]
-pub fn part2(input: &Vec<(i32, i32, i32, i32)>) -> usize {
-    input
-        .into_iter()
+pub fn part2(input: &str) -> usize {
+    input_generator(input)
         .filter(|(a1, a2, b1, b2)| i32::max(0, i32::min(*a2, *b2) + 1 - i32::max(*a1, *b1)) > 0)
         .count()
 }
@@ -40,11 +34,11 @@ mod tests {
 2-6,4-8";
     #[test]
     fn sample1() {
-        assert_eq!(part1(&input_generator(EXAMPLE)), 2);
+        assert_eq!(part1(EXAMPLE), 2);
     }
 
     #[test]
     fn sample2() {
-        assert_eq!(part2(&input_generator(EXAMPLE)), 4);
+        assert_eq!(part2(EXAMPLE), 4);
     }
 }
